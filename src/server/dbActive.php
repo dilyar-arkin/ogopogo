45961436fcc8218d72da97e474d6e1253a7b1366
Dilyar Arkin <blackcohn@gmail.com>
2023-10-26 00:59:36 -0700

    argument off

--- src/server/dbActive.php
+++ src/server/dbActive.php
@@ -2,11 +2,11 @@
     ini_set('display_errors', 'On'); 
     ini_set('html_errors', 0); 
     error_reporting(-1);
-    
+
     $connection = mysqli_init();
     mysqli_ssl_set($connection,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
     mysqli_real_connect($connection, "dilyar-db.mysql.database.azure.com", "DilyarArkin", "Yulghun987*", "ogopogo", 3306,MYSQLI_CLIENT_SSL);
-    if (mysqli_connect_errno($connection)) {
+    if (mysqli_connect_errno()) {
         die('Failed to connect to MySQL: '.mysqli_connect_error());
     }
     echo 'Successfully connected to MySQL.<br>';
